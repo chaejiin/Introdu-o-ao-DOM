e31da523d0fc228dd80cd30f28901c1223dce2cf
LEANDRO CHAEJIIN CASSIMIRO ASSANO <leandro.ccassano@senacsp.edu.br>
2025-05-19 17:28:30 -0300

    first commit

---
+++ js/scriptDOM.js
@@ -0,0 +1,21 @@
+function CalcularMedia() {
+    const nomeAluno = document.getElementById("nomeAluno").value;
+    const nota1 = parseFloat(document.getElementById("nota1").value);
+    const nota2 = parseFloat(document.getElementById("nota2").value);
+    const nota3 = parseFloat(document.getElementById("nota3").value);
+    const nota4 = parseFloat(document.getElementById("nota4").value);
+
+    let media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    document.getElementById("mediaAluno").value = media.toFixed(2);
+
+    if (media >= 7) {
+        document.getElementById("situacaoAluno").value = "Aprovado";
+    } else if (media >= 5) {
+        document.getElementById("situacaoAluno").value = "Em Recuperação";
+    } else {
+        document.getElementById("situacaoAluno").value = "Reprovado";
+
+    }
+
+} 
